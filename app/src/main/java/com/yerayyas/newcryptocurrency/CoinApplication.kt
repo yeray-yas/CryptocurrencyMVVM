8591a92be96cc556f58519619be2cc0b2e2ccca7
yeray-yas <yerayarucas@gmail.com>
2023-07-15 19:03:43 +0100

    it works

---
+++ app/src/main/java/com/yerayyas/newcryptocurrency/CoinApplication.kt
@@ -0,0 +1,7 @@
+package com.yerayyas.newcryptocurrency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application()
--- app/src/main/java/com/yerayyas/newcryptocurrency/presentation/MainActivity.kt
+++ app/src/main/java/com/yerayyas/newcryptocurrency/presentation/MainActivity.kt
@@ -3,44 +3,41 @@ package com.yerayyas.newcryptocurrency.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yerayyas.newcryptocurrency.presentation.coin_detail.CoinDetailScreen
+import com.yerayyas.newcryptocurrency.presentation.coin_list.CoinListScreen
 import com.yerayyas.newcryptocurrency.presentation.ui.theme.NEWCryptocurrencyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NEWCryptocurrencyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NEWCryptocurrencyTheme {
-        Greeting("Android")
     }
 }
